4651d25dfde52659cafd4729edfe72abe5123287
Blueform <Blueform@Blueform-compaq>
2018-02-07 11:35:24 +0100

    tabela

--- oferta.php
+++ oferta.php
@@ -4,7 +4,22 @@
     </head>
     <body>
 <?php
-
+echo '<table>';
+echo '<tr>';
+echo '<th>Asortyment</th>';
+echo '<th>Cena</th>';
+echo '<th>Zdjęcie</th>';
+echo '</tr>';
+$result = mysqli_query($conn, 'SELECT Asortyment, Cena, `Zdjęcie` FROM Oferta');
+while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) != NULL) {
+    echo '<tr>';
+    echo '<td>' . $row['Asortyment'] . '</td>';
+    echo '<td>' . $row['Cena'] . '</td>';
+    echo '<td>' . $row['Zdjęcie'] . '</td>';
+    echo '</tr>';
+}
+mysqli_free_result($result);
+echo '</table>';
 ?>
 
     </body>
