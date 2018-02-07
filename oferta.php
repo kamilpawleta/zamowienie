7d390f1d9fa08eb0d1f473d9e5cbf9fad6086809
Blueform <Blueform@Blueform-compaq>
2018-02-07 11:23:48 +0100

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml

--- oferta.php
+++ oferta.php
@@ -4,6 +4,18 @@
     </head>
     <body>
 <?php
+
+$conn = mysqli_connect('kamilpawleta.pl', 'pawletak', 'aIuPPm56', 'pawletak_db?zeroDateTimeBehavior=convertToNull', '3306');
+if (!$conn) {
+    die('Could not connect to MySQL: ' . mysqli_connect_error());
+}
+mysqli_query($conn, 'SET NAMES \'utf8\'');
+// TODO: insert your code here.
+mysqli_close($conn);
+
+
+
+
 ?>
 
     </body>
--- zamowienia.php
+++ zamowienia.php
@@ -6,7 +6,14 @@
 
 <?php
 
-echo "jestem";
+$conn = mysqli_connect('kamilpawleta.pl', 'pawletak', 'aIuPPm56', 'pawletak_db', '3306');
+if (!$conn) {
+    die('Could not connect to MySQL: ' . mysqli_connect_error());
+}
+mysqli_query($conn, 'SET NAMES \'utf8\'');
+// TODO: insert your code here.
+mysqli_close($conn);
+
 
 
 
