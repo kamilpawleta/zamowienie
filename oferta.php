06838af7e793b6cad061b71862b4765ceab64f65
Blueform <Blueform@Blueform-compaq>
2018-02-07 12:43:09 +0100

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml

--- oferta.php
+++ oferta.php
@@ -4,23 +4,25 @@
     </head>
     <body>
 <?php
-$conn1 = mysqli_connect('kamilpawleta.pl', 'pawletak', 'aIuPPm56', 'pawletak_db?zeroDateTimeBehavior=convertToNull', '3306');
-if (!$conn1) {
+$conn = mysqli_connect('kamilpawleta.pl', 'pawletak', 'aIuPPm56', 'pawletak_db', '3306');
+if (!$conn) {
     die('Could not connect to MySQL: ' . mysqli_connect_error());
 }
-mysqli_query($conn1, 'SET NAMES \'utf8\'');
+mysqli_query($conn, 'SET NAMES \'utf8\'');
 // TODO: insert your code here.
-mysqli_close($conn1);
+
 
 echo '<table>';
 echo '<tr>';
+echo '<th>id</th>';
 echo '<th>Asortyment</th>';
 echo '<th>Cena</th>';
 echo '<th>Zdjęcie</th>';
 echo '</tr>';
-$result = mysqli_query($conn, 'SELECT Asortyment, Cena, `Zdjęcie` FROM Oferta');
+$result = mysqli_query($conn, 'SELECT id, Asortyment, Cena, `Zdjęcie` FROM Oferta');
 while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) != NULL) {
     echo '<tr>';
+    echo '<td>' . $row['id'] . '</td>';
     echo '<td>' . $row['Asortyment'] . '</td>';
     echo '<td>' . $row['Cena'] . '</td>';
     echo '<td>' . $row['Zdjęcie'] . '</td>';
@@ -28,6 +30,8 @@ while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) != NULL) {
 }
 mysqli_free_result($result);
 echo '</table>';
+mysqli_close($conn);
+
 ?>
 
     </body>
