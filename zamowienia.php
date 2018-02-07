a3bc6921935e37febbd4ca71e7b40337f8577d68
Blueform <Blueform@Blueform-compaq>
2018-02-07 14:26:44 +0100

    Merge origin/master
    Conflicts:
    	nbproject/project.properties
    	nbproject/project.xml

--- zamowienia.php
+++ zamowienia.php
@@ -4,12 +4,29 @@
     </head>
     <body>
 
-<?php
+        <div class="container">
+            <form action="dane.php" method="post" name ="formularz">
+                Imię:
+                <input type="text" name="imie" class="form-control">
+                Nazwisko:
+                <input type="text" name="nazwisko" class="form-control">
+                Ulica:
+                <input type="text" name="ulica" class="form-control">
+                Nr domu:
+                <input type="number" name="dom" class="form-control">
+                Miejscowość:
+                <input type="text" name="miejscowosc" class="form-control">
 
-echo "jestem";
+                Róża <input type="checkbox" name="miejscowosc" class="form-control">
+                Kaktus <input type="checkbox" name="miejscowosc" class="form-control">
+                Tulipan <input type="checkbox" name="miejscowosc" class="form-control">
+                <button type="submit" class="btn btn-dark">Dodaj</button>
+                <div class="info alert alert-warning"></div>
+            </form>
+        </div>
 
 
-
-?>
+        <?php
+        ?>
     </body>
 </html>
